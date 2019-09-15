3f6462461667c4771df3af17b3359f623b11925d
Martin Slot <mas@impact.dk>
2019-09-15 15:27:50 +0200

    Initial commit

---
+++ src/Bl0g.ClusterHealth.WebApi/Bl0g.ClusterHealth.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bl0g.ClusterHealth.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Ping")]
+        public async Task<ActionResult> Ping()
+        {
+            return await Task.FromResult(Ok());
+        }
+    }
+}
